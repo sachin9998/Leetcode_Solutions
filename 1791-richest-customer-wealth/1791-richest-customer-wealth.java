bcaac84bd3019f16190496a0be5b22e12023b3fe
Sachin Alam <sachinalam4216@gmail.com>
2023-07-29 13:30:14 +0530

    Time: 0 ms (100%), Space: 42.4 MB (44.66%) - LeetHub

---
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+
+        int max = 0;
+        
+        for(int i = 0; i < accounts.length; i++) {
+            int count = 0;
+
+            for(int j = 0; j < accounts[i].length; j++) {
+                count = accounts[i][j] + count;
+            } 
+
+            if(count > max) {
+                max = count;
+            }
+
+        }
+
+        return max;
+        
+    }
+}
