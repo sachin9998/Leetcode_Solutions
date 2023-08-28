b9130faad29d7b9cd5cbade65f2638d9751bb177
Sachin Alam <sachinalam4216@gmail.com>
2023-08-28 12:06:17 +0530

    Time: 2 ms (100%), Space: 54.5 MB (50.42%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,34 +1,38 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
+        
+        // If the array has only one element, return the element itself as the maximum average
         if(nums.length == 1) {
             return (double) nums[0];
         }
 
+        // Calculate the initial average of the first k elements
         int sum = 0;
         for(int i = 0; i < k; i++) {
             sum += nums[i];
         }
 
+        // Set the initial maximum average to the calculated average
         int maxSum = sum;
 
-        // Sliding 
         int startIndex = 0;
         int endIndex = k;
         
         while(endIndex < nums.length) {
 
-            // Remmoove 
+            // Slide the window and update sum
             sum -= nums[startIndex];
             startIndex++;
 
             sum += nums[endIndex];
             endIndex++;
 
+            // Update maxSum by comparing with current subarray sum
             maxSum = Math.max(sum, maxSum);
 
         }
 
+        // Return average of maximum contigous subarray
         return (double) maxSum / k;
-
     }
 }
