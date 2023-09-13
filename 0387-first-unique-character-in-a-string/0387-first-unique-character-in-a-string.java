3942e5419e51a2db6e795a965999cdfb06b0e4ed
Sachin Alam <sachinalam4216@gmail.com>
2023-09-13 19:28:13 +0530

    Time: 5 ms (90.56%), Space: 44.5 MB (8.42%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,19 +1,19 @@
 class Solution {
     public int firstUniqChar(String s) {
-        
-        // Storing character and their count
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for(char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        // Base case...
+        if(s.length() == 0)  return -1;
+        // To keep track of the count of each character, we initialize an int[]store with size 26...
+        int[] store = new int[26];
+        // Traverse string to keep track number of times each character in the string appears...
+        for(char ch : s.toCharArray()){
+            store[ch - 'a']++;      // To access the store[] element representative of each character, we subtract ‘a’ from that character...
         }
-
-        for(int i = 0;  i < s.length(); i++) {
-            if(map.containsKey(s.charAt(i)) && map.get(s.charAt(i)) == 1) {
-                return i;
+        // Traverse string again to find a character that appears exactly one time, return it’s index...
+        for(int idx = 0; idx < s.length(); idx++){
+            if(store[s.charAt(idx) - 'a'] == 1){
+                return idx;
             }
         }
-
-        return -1;
+        return -1;      // if no character appeared exactly once...
     }
 }
