3b1ca069c2f7fa9ccf0c419d2179b00a9add08c0
Sachin Alam <sachinalam4216@gmail.com>
2023-10-18 20:40:44 +0530

    Time: 2 ms (57.98%), Space: 55.7 MB (48.43%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.java
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -1,23 +1,27 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        boolean isIncreasing = true;  // Indicates if the array is increasing
-        boolean isDecreasing = true;  // Indicates if the array is decreasing
 
-        // Check if the array is either increasing or non-increasing
-        for (int i = 1; i < nums.length; i++) {
-            // Check increasing condition.
-            if (nums[i] < nums[i - 1])
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
+
+        for(int i = 1; i < nums.length; i++) {
+
+            if(nums[i] < nums[i - 1]) {
                 isIncreasing = false;
+            }
 
-            // Check decreasing condition.
-            else if (nums[i] > nums[i - 1])
+            if(nums[i] > nums[i - 1]) {
                 isDecreasing = false;
+            }
 
-            // If it is neither increasing nor decreasing then don't continue the loop
-            if (!isIncreasing && !isDecreasing)
+            if(!isIncreasing && !isDecreasing) {
                 break;
+            }
+
+
         }
 
-        return isIncreasing || isDecreasing;  // Return true if either condition is met
+        return isIncreasing || isDecreasing;
+        
     }
 }
