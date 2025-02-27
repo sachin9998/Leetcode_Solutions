d8cc97e7be33897789c83f93b2771db9eebd4c5a
Sachin Alam <sachinalam4216@gmail.com>
2025-02-27 09:53:27 +0530

    Time: 5 ms (12.10%) | Memory: 41.4 MB (14.35%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
@@ -3,15 +3,15 @@ class Solution {
     public String toHex(int num) {
         if (num == 0) return "0";
 
-        // Hexadecimal characters map
+     
         char[] hexChars = "0123456789abcdef".toCharArray();
         StringBuilder hex = new StringBuilder();
 
-        // Process all 8 hex digits of a 32-bit number
+
         while (num != 0) {
-            int last4Bits = num & 15;          // Get last 4 bits
+            int last4Bits = num & 15;          
             System.out.println(last4Bits);
-            hex.append(hexChars[last4Bits]);  // Convert to hex and append
+            hex.append(hexChars[last4Bits]);  
             System.out.println(hexChars[10]);
             num >>>= 4;                       // Unsigned right shift by 4 bits
         }
