ac4d8aa8572207788da8c344895e5ea6d4fb2b1a
Sachin Alam <sachinalam4216@gmail.com>
2025-03-24 08:25:35 +0530

    Time: 64 ms (14.49%) | Memory: 45.9 MB (5.81%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,24 +1,19 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {     
         int n = nums.length;
-
         // Sort the given array
         Arrays.sort(nums);
 
         Set<List<Integer>> result = new HashSet<>();
-
         // Fix the first two elements and find the other two elements using two-pointer approach
-        for(int i = 0; i < n - 3; i++) {
-            
+        for(int i = 0; i < n - 3; i++) {    
             for(int j = i + 1; j < n - 2; j++) {
 
                 int start = j + 1;
                 int end = n - 1;
 
                 while(start < end) {
-                    
                     long sum =  (long) nums[i] + nums[j] + nums[start] + nums[end];
-
                     if(sum == target) {        
                         // Add the quadruplet in result and continue     
                         List<Integer> quad = new ArrayList<>();
@@ -26,7 +21,6 @@ class Solution {
                         quad.add(nums[start]);
                         quad.add(nums[j]);
                         quad.add(nums[end]);
-
                         result.add(quad);
                         start++;
                         end--;
