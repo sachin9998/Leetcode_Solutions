34b0eb12ccd5b4f12f052e8cbea416f06b94484d
Sachin Alam <sachinalam4216@gmail.com>
2025-11-02 23:12:29 +0530

    Time: 543 ms (21.25%) | Memory: 0B (100.00%) - LeetSync

--- 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -1,3 +1,3 @@
 /* Write your T-SQL query statement below */
 
-SELECT product_id from Products WHERE low_fats = 'Y' AND recyclable = 'Y'
+SELECT product_id from Products WHERE low_fats = 'Y' AND recyclable = 'Y';
