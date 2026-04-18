ad265ce0c2a727575381cf24a88888261cb9fdcf
Sachin Alam <sachinalam4216@gmail.com>
2026-04-18 13:08:33 +0530

    Time: 1965 ms (5.23%) | Memory: 12.5 MB (25.87%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        temp = x
+        num = 0
+    
+        while x > 0:
+            last = x % 10
+            num = last + num * 10
+            print("checking num", num)
+            x = x // 10
+
+        return temp == num
